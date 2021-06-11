b065f09cdf7a0c0ad3b91269b2e933c2548a6628
007Abdullah <kb337137@gmail.com>
2021-06-11 19:23:22 +0500

    working

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ elem.animate([
     { transform: 'translate(0,0)' },
     { transform: 'translate(600px,0)' },
     { transform: 'translate(0,0)' },
-    { transform: 'translate(600,0)' }
 ], {
-    duration: 1500
+    duration: 1500,
+    iterations: Infinity
 })
