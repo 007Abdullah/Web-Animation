fc2bdfd0d66736ef78cc027f2856f51edff36bfb
007Abdullah <kb337137@gmail.com>
2021-06-11 19:11:54 +0500

    animation function 2 arg

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 var elem = document.querySelector('#box');
 
-console.log(elem);
+elem.animate([
+    { transform: 'translate(0,0)' },
+    { transform: 'translate(600px,0)' },
+    { transform: 'translate(0,0)' },
+    { transform: 'translate(600,0)' }
+], {
+    duration: 1500
+})
